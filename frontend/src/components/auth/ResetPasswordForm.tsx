eea78c9880dfccc26edb553fab45fcdc6fae1408
Lutfiya Ainurrahman Prasetyo <lutfiyapr.creative@gmail.com>
2026-05-02 12:34:08 +0700

    feat: update ui for forgot password form

--- frontend/src/components/auth/ResetPasswordForm.tsx
+++ frontend/src/components/auth/ResetPasswordForm.tsx
@@ -4,42 +4,26 @@ import Input from "../form/input/InputField";
 
 export default function ResetPasswordForm() {
   return (
-    <div className="flex flex-col flex-1 w-full lg:w-1/2">
-      <div className="w-full max-w-md pt-10 mx-auto">
-        <Link
-          to="/"
-          className="inline-flex items-center text-sm text-gray-500 transition-colors hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300"
-        >
-          <svg
-            className="stroke-current"
-            xmlns="http://www.w3.org/2000/svg"
-            width="20"
-            height="20"
-            viewBox="0 0 20 20"
-            fill="none"
-          >
-            <path
-              d="M12.7083 5L7.5 10.2083L12.7083 15.4167"
-              stroke=""
-              strokeWidth="1.5"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-          Back to dashboard
-        </Link>
-      </div>
+    <div className="flex flex-col flex-1 w-full overflow-y-auto no-scrollbar">
       <div className="flex flex-col justify-center flex-1 w-full max-w-md mx-auto">
-        <div className="mb-5 sm:mb-8">
-          <h1 className="mb-2 font-semibold text-gray-800 text-title-sm dark:text-white/90 sm:text-title-md">
-            Forgot Your Password?
-          </h1>
-          <p className="text-sm text-gray-500 dark:text-gray-400">
-            Enter the email address linked to your account, and we’ll send you a
-            link to reset your password.
-          </p>
-        </div>
         <div>
+          <Link to="/" className="flex justify-center mb-4">
+            <img
+              width={80}
+              height={18}
+              src="/images/logo/app-logo.png"
+              alt="Logo"
+            />
+          </Link>
+          <div className="mb-5 sm:mb-8">
+            <h1 className="mb-2 font-semibold text-gray-800 text-title-sm dark:text-white/90 sm:text-title-md">
+              Lupa Password Anda?
+            </h1>
+            <p className="text-sm text-gray-500 dark:text-gray-400">
+              Masukkan alamat email yang terhubung dengan akun Anda, dan kami
+              akan mengirimkan tautan untuk mengatur ulang password Anda.
+            </p>
+          </div>
           <form>
             <div className="space-y-5">
               {/* <!-- Email --> */}
@@ -51,26 +35,26 @@ export default function ResetPasswordForm() {
                   type="email"
                   id="email"
                   name="email"
-                  placeholder="Enter your email"
+                  placeholder="Masukkan email anda"
                 />
               </div>
 
               {/* <!-- Button --> */}
               <div>
                 <button className="flex items-center justify-center w-full px-4 py-3 text-sm font-medium text-white transition rounded-lg bg-brand-500 shadow-theme-xs hover:bg-brand-600">
-                  Send Reset Link
+                  Kirim Tautan Reset
                 </button>
               </div>
             </div>
           </form>
           <div className="mt-5">
             <p className="text-sm font-normal text-center text-gray-700 dark:text-gray-400 sm:text-start">
-              Wait, I remember my password...
+              Tunggu, saya ingat password saya...
               <Link
-                to="/"
+                to="/signin"
                 className="text-brand-500 hover:text-brand-600 dark:text-brand-400"
               >
-                Click here
+                Klik di sini
               </Link>
             </p>
           </div>
